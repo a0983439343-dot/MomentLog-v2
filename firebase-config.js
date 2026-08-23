ec54ae7c62d5df072fadc44d5f42318365086c80
a0983439343-dot <a0983439343@gmail.com>
2026-08-23 13:15:01 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/12.17.0/firebase-app-compat.js"
-);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.0/firebase-database.js";
 
-importScripts(
-  "https://www.gstatic.com/firebasejs/12.17.0/firebase-messaging-compat.js"
-);
-
-firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBCMPxNB58yeF5zUxQmSlL_MjSGN8VR7YY",
   authDomain: "momentlog-afcbf.firebaseapp.com",
   databaseURL: "https://momentlog-afcbf-default-rtdb.firebaseio.com",
@@ -15,71 +11,10 @@ firebase.initializeApp({
   messagingSenderId: "585186639693",
   appId: "1:585186639693:web:9161128d29688044950104",
   measurementId: "G-9QP570QV5X"
-});
+};
 
-const messaging = firebase.messaging();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
 
-messaging.onBackgroundMessage((payload) => {
-  const title =
-    payload?.data?.title ||
-    payload?.notification?.title ||
-    "MomentLog";
-
-  const body =
-    payload?.data?.body ||
-    payload?.notification?.body ||
-    "該記錄了！";
-
-  self.registration.showNotification(
-    title,
-    {
-      body,
-      icon: "./icon-192.png",
-      badge: "./icon-192.png",
-      tag:
-        payload?.data?.tag ||
-        "momentlog-reminder",
-      data: {
-        url: "./"
-      }
-    }
-  );
-});
-
-self.addEventListener(
-  "notificationclick",
-  (event) => {
-
-    event.notification.close();
-
-    const targetUrl =
-      new URL(
-        event.notification?.data?.url || "./",
-        self.location.origin
-      ).href;
-
-    event.waitUntil(
-      clients
-        .matchAll({
-          type: "window",
-          includeUncontrolled: true
-        })
-        .then((clientList) => {
-
-          for (const client of clientList) {
-            if ("focus" in client) {
-              return client.focus();
-            }
-          }
-
-          if ("openWindow" in clients) {
-            return clients.openWindow(
-              targetUrl
-            );
-          }
-
-          return undefined;
-        })
-    );
-  }
-);
+export { app, auth, database };
