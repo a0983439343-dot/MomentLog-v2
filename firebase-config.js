5e94ae32a1e0e90bac7fac33e302b85d7756ed15
a0983439343-dot <a0983439343@gmail.com>
2026-08-23 12:23:47 +0800

    Add Firebase messaging setup with background notifications

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,12 @@
-const firebaseConfig = {
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.17.0/firebase-app-compat.js"
+);
+
+importScripts(
+  "https://www.gstatic.com/firebasejs/12.17.0/firebase-messaging-compat.js"
+);
+
+firebase.initializeApp({
   apiKey: "AIzaSyBCMPxNB58yeF5zUxQmSlL_MjSGN8VR7YY",
   authDomain: "momentlog-afcbf.firebaseapp.com",
   databaseURL: "https://momentlog-afcbf-default-rtdb.firebaseio.com",
@@ -7,5 +15,71 @@ const firebaseConfig = {
   messagingSenderId: "585186639693",
   appId: "1:585186639693:web:9161128d29688044950104",
   measurementId: "G-9QP570QV5X"
-};
-export default firebaseConfig;
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title =
+    payload?.data?.title ||
+    payload?.notification?.title ||
+    "MomentLog";
+
+  const body =
+    payload?.data?.body ||
+    payload?.notification?.body ||
+    "該記錄了！";
+
+  self.registration.showNotification(
+    title,
+    {
+      body,
+      icon: "./icon-192.png",
+      badge: "./icon-192.png",
+      tag:
+        payload?.data?.tag ||
+        "momentlog-reminder",
+      data: {
+        url: "./"
+      }
+    }
+  );
+});
+
+self.addEventListener(
+  "notificationclick",
+  (event) => {
+
+    event.notification.close();
+
+    const targetUrl =
+      new URL(
+        event.notification?.data?.url || "./",
+        self.location.origin
+      ).href;
+
+    event.waitUntil(
+      clients
+        .matchAll({
+          type: "window",
+          includeUncontrolled: true
+        })
+        .then((clientList) => {
+
+          for (const client of clientList) {
+            if ("focus" in client) {
+              return client.focus();
+            }
+          }
+
+          if ("openWindow" in clients) {
+            return clients.openWindow(
+              targetUrl
+            );
+          }
+
+          return undefined;
+        })
+    );
+  }
+);
